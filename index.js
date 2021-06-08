6f4336485a93d5981276b8665edd9dc23b8fe2d6
elefe <eleferrets@gmail.com>
2021-06-08 18:50:04 -0400

    Added popcorn icon and added basic skeleton

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get selectors for the div
+let std_name = document.querySelector("#student-name");
+let std_advisor = document.querySelector("#student-advisor");
+// Add listener for button to fetch api
+fall_sem.addEventListener("click", (a) => {
+    /*
+    semester = "Fall Semester";
+    updateReportCard();
+    toggleDropdown();*/
+    // Fetch movies
+});
